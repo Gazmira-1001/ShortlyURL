85470aecd3a8f1fa5b4545e1a1163e6a51e0ac2c
Gazmira <gazmira.brahushi1@gmail.com>
2024-03-22 10:55:09 -0400

    Add project files.

---
+++ ShortlyClient-Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShortlyClient_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
